bd3e3dda1a416147313e2de05fdf10945767b159
Elias M <emedina21@alumnos.uaq.mx>
2022-05-19 14:37:03 -0500

    Multiplicacion Escalar y comparar compatiblidad

--- MatricesJava/src/Matrix/MathMatrix.java
+++ MatricesJava/src/Matrix/MathMatrix.java
@@ -6,10 +6,14 @@ public class MathMatrix {
 				Matrix_A.getDimensiones()[1] == Matrix_B.getDimensiones()[1] ;
 	}
 	
+	public static boolean CompareCompatibility(Matrix matrixA,Matrix matrixB) {
+		return	matrixA.getDimensiones()[1] == matrixB.getDimensiones()[0];
+	}
+	
 	public static Matrix MatrixSum(Matrix MatrixA, Matrix MatrixB) 
 	{
 		if(!compareDim(MatrixA,MatrixB)) {
-			throw new Error("Matrix Not Equasl Dimensions");
+			throw new Error("Matrixes Don't Haave Equal Dimensions");
 		}else 
 		{
 			Matrix matrix_aux = new Matrix(MatrixA.filas, MatrixB.columnas, "aux");
@@ -24,6 +28,27 @@ public class MathMatrix {
 
 	}
 
+	public static Matrix MultiplicacionEscalar(Matrix MatrixA,int escalar) {
+		Matrix matrix_aux = new Matrix(MatrixA.filas,MatrixA.columnas,"aux");
+		for(int i = 0; i<MatrixA.filas; i++) {
+			for(int j = 0; j<MatrixA.columnas; j++) {
+				matrix_aux.content[i][j] = matrix_aux.content[i][j]*escalar;
+			}
+		}
+		return matrix_aux;
+	}
+	
+	public static Matrix MultiplicacionMatrix(Matrix MatrixA, Matrix MatrixB) {
+		Matrix matrix_aux = new Matrix(MatrixA.filas, MatrixB.columnas,"aux");
+		if(!CompareCompatibility(MatrixA, MatrixB)) {
+			throw new Error("Matrixes Are Not Compatible, Thus This Can't Be Done");
+		}
+		else{
+			
+		}
+		return matrix_aux;
+	}
+	
 	public static Matrix MatrixDifferences(Matrix MatrixA, Matrix MatrixB) 
 	{
 		if(!compareDim(MatrixA,MatrixB)) {
