8d6f67ef74452493bd22cc367e152f2d1668d936
Elias M <emedina21@alumnos.uaq.mx>
2022-05-20 20:44:59 -0500

    Agregue main

--- MatricesJava/src/App/Main.java
+++ MatricesJava/src/App/Main.java
@@ -8,16 +8,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Matrix mi_matrix1 = new Matrix("pepe", new double[][] {{1,2,3},{4,5,6},{7,8,9}} ) ;
+		Matrix mi_matrix1 = new Matrix("pepe", new double[][] {{1,0},{1,1},{1,1}} ) ;
 		Matrix mi_matrix2 = new Matrix("juan", new double[][] {{1,0,1},{1,1,1},{0,0,0}} ) ;
+		mi_matrix1.ShowMatrix();
+		System.out.println();
+		mi_matrix2.ShowMatrix();
+		Matrix mi_matrix3 = MathMatrix.MakePowerOf2(mi_matrix2);
+		System.out.println();
+		mi_matrix3.ShowMatrix();
 		
 		
 		
-		
-		
-		System.out.println(
-				Arrays.deepToString(( MathMatrix.MatrixSum(mi_matrix1, mi_matrix2)).getContent()
-				));
 	
 	}
 
